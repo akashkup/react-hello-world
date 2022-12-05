444b66c82e36fd2dfba3352e7b78732429b06217
akashkup <66254304+akashkup@users.noreply.github.com>
2022-12-05 21:34:45 +0530

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ const Title = () => {
   return (
     <div className="title">
       <span className="caption">
-        Hello <strong>World</strong>. I will acheive my goals.Target 2025.
+        Hello <strong>World</strong>. I will acheive my goals.Target 2025. I have started to acheive my goals
       </span>
     </div>
   )
